c3f27de7c5aa88eadbc79910a898fbd6495d6cc5
Yongsheng <ygong56@uwo.ca>
2024-05-20 00:32:03 -0400

    I created an array and selectors variables for index-page.js

---
+++ scripts/index-page.js
@@ -0,0 +1,25 @@
+let userCoomment = [
+    {
+        name: "Victor Pinto",
+        timeStamp: "11/02/2023",
+        commentText: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+    },
+    {
+        name: "Christina Cabrera",
+        timeStamp: "10/28/2023",
+        commentText:"I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+    },
+    {
+        name: "Isaac Tadesse",
+        timeStamp: "10/20/2023",
+        commentText:"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+    }
+];
+
+const name=document.querySelector(".comment__display-post-content-title");
+const timeStamp=document.querySelector(".comment__display-post-content-date");
+const commentText=document.querySelector(".comment__display-post-content-paragraph");
+function addComment() {
+
+}
+addComment();
